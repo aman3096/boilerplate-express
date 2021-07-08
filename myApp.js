53eb84842ef52e8d80cd882169531cd18bfb5916
Aman Tandon <aman.tandon3096@gmail.com>
2021-07-08 16:38:40 +0530

    feat: using express.static() middleware

--- myApp.js
+++ myApp.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express();
-console.log('Hello World');
+console.log('Hello world')
 let absolutePath = __dirname + '/views/index.html'
+let assetsPath = __dirname + '/public'
 app.get("/", (req,res)=>{
     res.sendFile(absolutePath)
 })
 
-
-
-
-
-
+app.use("/public",express.static(assetsPath))
 
 
 
