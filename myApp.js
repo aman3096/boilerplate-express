a6ce25bc9f3ee2b082d149150e4ee117fb832c29
Aman Tandon <aman.tandon3096@gmail.com>
2021-07-08 16:29:51 +0530

    feat: serve HTML file using sendFile(path)

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 console.log('Hello World');
-app.get( '/', (req,res)=>{
-    res.send('Hello Express')
+let absolutePath = __dirname + '/views/index.html'
+app.get("/", (req,res)=>{
+    res.sendFile(absolutePath)
 })
+
 
 
 
